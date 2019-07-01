886584e5cc201b43a2b5781e1a14bf804ce1709e
GulshanN <nihalani_gulshan@yahoo.com>
2019-07-01 18:58:24 +0530

    getting Started Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import {FormsModule} from '@angular/forms'
+
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./Product/product-list.component";
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+
+@NgModule(
+  {
+    imports:[BrowserModule,FormsModule],
+    declarations:[AppComponent,ProductListComponent,ConvertToSpacesPipe],
+    bootstrap:[AppComponent]
+  }
+)
+export class AppModule{
+
+}
